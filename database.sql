776a104d8a5a5d270e71293f6b6cf01e77e03fc4
tolis9981 <apostolos1998@hotmail.com>
2023-01-09 22:30:46 +0200

    sd

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 09, 2023 at 09:29 PM
+-- Generation Time: Jan 09, 2023 at 09:30 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
